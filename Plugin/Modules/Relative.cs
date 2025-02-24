aabbef25276e06e825a13d852ffd2dfefd87fac3
fixfactory <5952846+fixfactory@users.noreply.github.com>
2025-02-24 08:54:56 -0800

    Added properties for Relative overlay

--- Plugin/Modules/Relative.cs
+++ Plugin/Modules/Relative.cs
@@ -18,7 +18,10 @@
 
 using GameReaderCommon;
 using SimHub.Plugins;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace benofficial2.Plugin
 {
@@ -46,24 +49,153 @@ namespace benofficial2.Plugin
         }
     }
 
+    public class RelativeRow
+    {
+        public bool RowVisible { get; set; } = false;
+        public int PositionInClass { get; set; } = 0;
+        public string ClassColor { get; set; } = string.Empty;
+        public string ClassTextColor { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public bool OutLap { get; set; } = false;
+        public int iRating { get; set; } = 0;
+        public string iRatingText { get; set; } = string.Empty;
+        public string License { get; set; } = string.Empty;
+        public double SafetyRating { get; set; } = 0;
+        public double GapToPlayer { get; set; } = 0;
+    }
+
+    public class RelativeAhead
+    {
+        public const int MaxRows = 3;
+        public List<RelativeRow> Rows { get; internal set; }
+
+        public RelativeAhead()
+        {
+            Rows = new List<RelativeRow>(Enumerable.Range(0, MaxRows).Select(x => new RelativeRow()));
+        }
+    }
+    public class RelativeBehind
+    {
+        public const int MaxRows = 3;
+        public List<RelativeRow> Rows { get; internal set; }
+
+        public RelativeBehind()
+        {
+            Rows = new List<RelativeRow>(Enumerable.Range(0, MaxRows).Select(x => new RelativeRow()));
+        }
+    }
+
     public class Relative : IPluginModule
     {
+        private DateTime _lastUpdateTime = DateTime.MinValue;
+        private TimeSpan _updateInterval = TimeSpan.FromMilliseconds(500);
+
         public RelativeSettings Settings { get; set; }
+
+        public RelativeAhead Ahead;
+        public RelativeBehind Behind;
+
+        public Relative()
+        {
+            Ahead = new RelativeAhead();
+            Behind = new RelativeBehind();
+        }
 
         public void Init(PluginManager pluginManager, benofficial2 plugin)
         {
             Settings = plugin.ReadCommonSettings<RelativeSettings>("RelativeSettings", () => new RelativeSettings());
             plugin.AttachDelegate(name: "Relative.BackgroundOpacity", valueProvider: () => Settings.BackgroundOpacity);
+
+            InitRelative(plugin, "Ahead", Ahead.Rows, RelativeAhead.MaxRows);
+            InitRelative(plugin, "Behind", Behind.Rows, RelativeBehind.MaxRows);
+        }
+
+        private void InitRelative(benofficial2 plugin, string aheadBehind, List<RelativeRow> rows, int maxRows)
+        {
+            for (int rowIdx = 0; rowIdx < maxRows; rowIdx++)
+            {
+                RelativeRow row = rows[rowIdx];
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.RowVisible", valueProvider: () => row.RowVisible);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.PositionInClass", valueProvider: () => row.PositionInClass);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.ClassColor", valueProvider: () => row.ClassColor);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.ClassTextColor", valueProvider: () => row.ClassTextColor);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.Number", valueProvider: () => row.Number);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.Name", valueProvider: () => row.Name);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.OutLap", valueProvider: () => row.OutLap);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.iRating", valueProvider: () => row.iRating);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.iRatingText", valueProvider: () => row.iRatingText);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.License", valueProvider: () => row.License);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.SafetyRating", valueProvider: () => row.SafetyRating);
+                plugin.AttachDelegate(name: $"Relative.{aheadBehind}{rowIdx:00}.GapToPlayer", valueProvider: () => row.GapToPlayer);
+            }
         }
 
         public void DataUpdate(PluginManager pluginManager, benofficial2 plugin, ref GameData data)
         {
+            if (DateTime.Now - _lastUpdateTime < _updateInterval) return;
+            _lastUpdateTime = DateTime.Now;
 
+            UpdateRelative(ref data, Ahead.Rows, RelativeAhead.MaxRows, data.NewData.OpponentsAheadOnTrack);
+            UpdateRelative(ref data, Behind.Rows, RelativeBehind.MaxRows, data.NewData.OpponentsBehindOnTrack);
+        }
+
+        public void UpdateRelative(ref GameData data, List<RelativeRow> rows, int maxRows, List<Opponent> opponents)
+        {
+            for (int rowIdx = 0; rowIdx < maxRows; rowIdx++)
+            {
+                RelativeRow row = rows[rowIdx];
+
+                if (rowIdx >= opponents.Count)
+                {
+                    BlankRow(row);
+                    continue;
+                }
+
+                Opponent opponent = opponents[rowIdx];
+                if (!IsValidRow(opponent))
+                {
+                    BlankRow(row);
+                    continue;
+                }
+
+                row.RowVisible = true;
+                row.PositionInClass = opponent.PositionInClass;
+                row.ClassColor = opponent.CarClassColor;
+                row.ClassTextColor = opponent.CarClassTextColor;
+                row.Number = opponent.CarNumber;
+                row.Name = opponent.Name;
+                row.OutLap = opponent.IsOutLap; // TODO use driverModule
+                row.iRating = (int)opponent.IRacing_IRating;
+                row.iRatingText = DriverModule.FormatIRating(row.iRating);
+                (row.License, row.SafetyRating) = DriverModule.ParseLicenseString(opponent.LicenceString);
+                row.GapToPlayer = opponent.GaptoPlayer ?? 0;
+            }
         }
 
         public void End(PluginManager pluginManager, benofficial2 plugin)
         {
             plugin.SaveCommonSettings("RelativeSettings", Settings);
+        }
+
+        public void BlankRow(RelativeRow row)
+        {
+            row.RowVisible = false;
+            row.PositionInClass = 0;
+            row.ClassColor = string.Empty;
+            row.ClassTextColor = string.Empty;
+            row.Number = string.Empty;
+            row.Name = string.Empty;
+            row.OutLap = false;
+            row.iRating = 0;
+            row.iRatingText = string.Empty;
+            row.License = string.Empty;
+            row.SafetyRating = 0;
+            row.GapToPlayer = 0;
+        }
+        public bool IsValidRow(Opponent opponent)
+        {
+            return true;
         }
     }
 }
