ef11cad971511f7b27a444c938818b18d54909a1
fixfactory <5952846+fixfactory@users.noreply.github.com>
2025-03-30 16:57:59 -0700

    Change back update rate to 1 second

--- Plugin/Modules/FuelCalc.cs
+++ Plugin/Modules/FuelCalc.cs
@@ -45,7 +45,7 @@ namespace benofficial2.Plugin
         private DriverModule _driverModule = null;
 
         private DateTime _lastUpdateTime = DateTime.MinValue;
-        private TimeSpan _updateInterval = TimeSpan.FromMilliseconds(2000);
+        private TimeSpan _updateInterval = TimeSpan.FromMilliseconds(1000);
 
         public FuelCalcSettings Settings { get; set; }
         public bool Visible { get; internal set; } = true;
