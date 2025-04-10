79374a3bf87b941f7d62b7c24828f1cb056a8354
fixfactory <5952846+fixfactory@users.noreply.github.com>
2025-04-10 14:53:08 -0700

    Added option for 24-hour clock format (thanks L061N)

--- Plugin/Modules/General.cs
+++ Plugin/Modules/General.cs
@@ -25,6 +25,7 @@ namespace benofficial2.Plugin
     public class GeneralSettings : ModuleSettings
     {
         public bool CheckForUpdates { get; set; } = true;
+        public bool ClockFormat24h { get; set; } = true;
     }
 
     public class GeneralModule : PluginModuleBase
@@ -36,6 +37,7 @@ namespace benofficial2.Plugin
             // There was an issue with loading settings stored pre-3.0, so we need to specify a new settings key
             Settings = plugin.ReadCommonSettings<GeneralSettings>("GeneralSettings_3.1", () => new GeneralSettings());
             plugin.AttachDelegate(name: "CheckForUpdates", valueProvider: () => Settings.CheckForUpdates);
+            plugin.AttachDelegate(name: "ClockFormat24h", valueProvider: () => Settings.ClockFormat24h);
         }
 
         public override void DataUpdate(PluginManager pluginManager, benofficial2 plugin, ref GameData data)
