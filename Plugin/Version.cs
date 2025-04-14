f173a0b96e76c2bbf2ac99896d23de87fdba28b0
fixfactory <5952846+fixfactory@users.noreply.github.com>
2025-04-14 06:37:04 -0700

    Bumped version to 3.3

--- Plugin/Version.cs
+++ Plugin/Version.cs
@@ -29,7 +29,7 @@ namespace benofficial2.Plugin
         private const string _versionUrl = "https://raw.githubusercontent.com/fixfactory/bo2-official-overlays/main/Versions.json";
         private const string _downloadPageUrl = "https://github.com/fixfactory/bo2-official-overlays/releases";
 
-        public const string CurrentVersion = "3.2";
+        public const string CurrentVersion = "3.3";
         
         public async Task CheckForUpdateAsync()
         {
