4f2caf76e3f0fac91ae0773604733291d6baa03a
Fix Factory <5952846+fixfactory@users.noreply.github.com>
2025-08-06 11:55:22 -0700

    Bumped versions

--- Plugin/Version.cs
+++ Plugin/Version.cs
@@ -30,7 +30,7 @@ namespace benofficial2.Plugin
         private const string _downloadPageUrl = "https://github.com/fixfactory/bo2-official-overlays/releases";
 
         public const string CurrentVersion = "3.8";
-        public const string BetaVersion = "1";
+        public const string BetaVersion = "2";
         
         static public bool IsBetaVersion
         {
