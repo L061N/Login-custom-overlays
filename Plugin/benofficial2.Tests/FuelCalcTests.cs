89675c9e361fca5f898adedf3ba116bdd7d8a69d
Fix Factory <5952846+fixfactory@users.noreply.github.com>
2025-08-15 03:47:34 -0700

    Added unit test project.

---
+++ Plugin/benofficial2.Tests/FuelCalcTests.cs
@@ -0,0 +1,13 @@
+﻿namespace benofficial2.Tests
+{
+    [TestClass]
+    public sealed class FuelCalcTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = 5;
+            Assert.AreEqual(5, result);
+        }
+    }
+}
