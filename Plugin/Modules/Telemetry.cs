0b4ebbc8364f8b312ec6609edeb2a0fb6273545e
Fix Factory <5952846+fixfactory@users.noreply.github.com>
2025-09-12 04:35:28 -0700

    Added support for other games. Fixed handbrake gauge.

--- Plugin/Modules/Telemetry.cs
+++ Plugin/Modules/Telemetry.cs
@@ -40,22 +40,7 @@ namespace benofficial2.Plugin
 
     public class TelemetryModule : PluginModuleBase
     {
-        private const int _tracesWidth = 500;
-        private const int _tracesLeft = 17;
-        private const int _pedalsLeft = 492;
-        private const int _steeringLeft = 650;
-        private const int _gearAndSpeedLeft = 573;
-        private const int _backgroundWidth = 760;
-
         public TelemetrySettings Settings { get; set; }
-
-        public int PedalsLeft { get; set; } = _pedalsLeft;
-
-        public int SteeringLeft { get; set; } = _steeringLeft;
-
-        public int GearAndSpeedLeft { get; set; } = _gearAndSpeedLeft;
-
-        public int BackgroundWidth { get; set; } = _backgroundWidth;
 
         public override void Init(PluginManager pluginManager, benofficial2 plugin)
         {
@@ -68,50 +53,14 @@ namespace benofficial2.Plugin
             plugin.AttachDelegate(name: "Telemetry.GuideLinesVisible", valueProvider: () => Settings.GuideLinesVisible);
             plugin.AttachDelegate(name: "Telemetry.PedalsVisible", valueProvider: () => Settings.PedalsVisible);
             plugin.AttachDelegate(name: "Telemetry.PedalsValuesVisible", valueProvider: () => Settings.PedalsValuesVisible);
-            plugin.AttachDelegate(name: "Telemetry.PedalsLeft", valueProvider: () => PedalsLeft);
             plugin.AttachDelegate(name: "Telemetry.SteeringVisible", valueProvider: () => Settings.SteeringVisible);
-            plugin.AttachDelegate(name: "Telemetry.SteeringLeft", valueProvider: () => SteeringLeft);
             plugin.AttachDelegate(name: "Telemetry.GearAndSpeedVisible", valueProvider: () => Settings.GearAndSpeedVisible);
-            plugin.AttachDelegate(name: "Telemetry.GearAndSpeedLeft", valueProvider: () => GearAndSpeedLeft);
             plugin.AttachDelegate(name: "Telemetry.ShiftLightsVisible", valueProvider: () => Settings.ShiftLightsVisible);
             plugin.AttachDelegate(name: "Telemetry.BackgroundOpacity", valueProvider: () => Settings.BackgroundOpacity);
-            plugin.AttachDelegate(name: "Telemetry.BackgroundWidth", valueProvider: () => BackgroundWidth);
         }
 
         public override void DataUpdate(PluginManager pluginManager, benofficial2 plugin, ref GameData data)
         {
-            int tracesOffset = 0;
-            if (!Settings.TracesVisible)
-            {
-                tracesOffset = -_tracesWidth + _tracesLeft;
-            }
-            else
-            {
-                tracesOffset = -(_tracesWidth - Settings.TracesWidth);
-            }
-
-            int pedalsOffset = 0;
-            if (!Settings.PedalsVisible)
-            {
-                pedalsOffset = -(_gearAndSpeedLeft - _pedalsLeft);
-            }
-
-            int gearAndSpeedOffset = 0;
-            if (!Settings.GearAndSpeedVisible)
-            {
-                gearAndSpeedOffset = -(_steeringLeft - _gearAndSpeedLeft);
-            }
-
-            int steeringOffset = 0;
-            if (!Settings.SteeringVisible)
-            {
-                steeringOffset = -(_backgroundWidth - _steeringLeft);
-            }
-
-            PedalsLeft = _pedalsLeft + tracesOffset;
-            GearAndSpeedLeft = _gearAndSpeedLeft + tracesOffset + pedalsOffset;
-            SteeringLeft = _steeringLeft + tracesOffset + pedalsOffset + gearAndSpeedOffset;
-            BackgroundWidth = _backgroundWidth + tracesOffset + pedalsOffset + gearAndSpeedOffset + steeringOffset;
         }
 
         public override void End(PluginManager pluginManager, benofficial2 plugin)
