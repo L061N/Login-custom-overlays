24442d7fad2ac0b4f7cf02bb7362aac603bcfcec
Fix Factory <5952846+fixfactory@users.noreply.github.com>
2025-11-19 04:31:27 -0800

    Bumped version to 4.1 (beta1) Bumped minimum SimHub version

--- Plugin/Version.cs
+++ Plugin/Version.cs
@@ -29,8 +29,8 @@ namespace benofficial2.Plugin
         private const string _versionUrl = "https://raw.githubusercontent.com/fixfactory/bo2-official-overlays/main/Versions.json";
         private const string _downloadPageUrl = "https://github.com/fixfactory/bo2-official-overlays/releases";
 
-        public const string CurrentVersion = "4.0";
-        public const string BetaVersion = "";
+        public const string CurrentVersion = "4.1";
+        public const string BetaVersion = "1";
         
         static public bool IsBetaVersion
         {
